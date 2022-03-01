de495e7f553e248f89b10c2c45485fb18126965d
sumitguptas31 <sumitguptarosera@gmail.com>
2022-03-01 23:25:24 +0530

    mongodb-assignment

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -3,15 +3,10 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema( {
     bookName: String, 
     authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+     catogary: String,
+     year : Number,
+})
+
 
 
 module.exports = mongoose.model('Book', bookSchema) //users
