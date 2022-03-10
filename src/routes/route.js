fb97bb0cc11bb8b96f8b5142a88d90a9ae06fc02
sumitguptas31 <sumitguptarosera@gmail.com>
2022-03-11 03:14:38 +0530

    status code

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+const userController= require("../controllers/userController")
+const MiddleWare= require("../Middleware/middleware")
+
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+router.post("/users", userController.createUser  )
 
+router.post("/login", userController.loginUser)
 
+//The userId is sent by front end
+router.get("/users/:userId",MiddleWare.createmidd,MiddleWare.authorise, userController.getUserData)
 
-router.post("/createBook", BookController.createBook  )
+router.put("/users/:userId", MiddleWare.createmidd,MiddleWare.authorise,userController.updateUser)
+router.delete("/deleteuser/:userId",MiddleWare.createmidd,MiddleWare.authorise,userController.deleteUser)
+router.post("/postupdate/:userId/posts",MiddleWare.createmidd,MiddleWare.authorise,userController.postMessage)
+
+module.exports = router;
+
 
 
 
@@ -44,14 +53,6 @@ router.post("/createBook", BookController.createBook  )
 // router.get('/termsAndConditions',  UserController.termsAndConditions)
 // router.get('/register',  UserController.register)
 
-
-
-
-
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
-
-
-
 // router.get("/basicRoute2", commonMW.mid1, UserController.basicCode2)
 // router.get("/basicRoute3", commonMW.mid2, UserController.basicCode3)
 // router.get("/basicRoute4", commonMW.mid1, commonMW.mid4, UserController.basicCode4)
@@ -59,4 +60,3 @@ router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.
 
 
 
-module.exports = router;
